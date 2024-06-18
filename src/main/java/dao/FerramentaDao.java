055d5df42ed1d863ce7bd5549694f141b6e9a900
Gustavoaals <gustavoalvess842@gmail.com>
2024-06-18 19:04:06 -0300

    inicio do projeto

--- src/main/java/dao/FerramentaDao.java
+++ src/main/java/dao/FerramentaDao.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
 
-/**
- *
- * @author Gustavo
- */
+
+import java.util.ArrayList;
+import modelo.Ferramenta;
+
+
 public class FerramentaDao {
+
+
+    public static ArrayList<Ferramenta> minhaLista = new ArrayList<>();
+   
+   
+    public static ArrayList<Ferramenta> getMinhaLista() {
+        return minhaLista;
+    }
+
+   
+    public static void setMinhaLista(ArrayList<Ferramenta> minhaLista) {
+        FerramentaDao.minhaLista = minhaLista;
+    }
+
     
+    public static int maiorID() {
+        int maiorID = 0;
+        for (int i = 0; i < minhaLista.size(); i++) {
+            if (minhaLista.get(i).getId_ferramenta() > maiorID) {
+                maiorID = minhaLista.get(i).getId_ferramenta();
+            }
+        }
+        return maiorID;
+    }
 }
---
+++ src/main/java/modelo/Ferramenta.java
@@ -0,0 +1,11 @@
+package modelo;
+
+import dao.FerramentaDao;
+import java.util.ArrayList;
+
+public class Ferramenta{
+    private int id_ferramenta;
+    private String nome;
+    private String marca;
+    private float custo;
+}
