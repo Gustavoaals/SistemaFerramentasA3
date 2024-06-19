f5912c2579af9d4ef253c5d0aa115c7538fbdfc1
Gustavoaals <gustavoalvess842@gmail.com>
2024-06-19 11:01:12 -0300

    inicio do projeto

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,11 +1,12 @@
+
 package principal;
 
-import java.sql.Connection;
+import java.sql.Connection; 
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import visao.FRMCentral;
 
-public class Principal {
+public class Principal  {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/sistemaemprestimoa3";
         String user = "root";
