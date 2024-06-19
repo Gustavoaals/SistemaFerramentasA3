87d5342b5946dddbdb534178918baf4fe4beefe6
Gustavoaals <gustavoalvess842@gmail.com>
2024-06-18 21:14:57 -0300

    inicio do projeto

--- src/main/java/modelo/Emprestimo.java
+++ src/main/java/modelo/Emprestimo.java
@@ -11,3 +11,12 @@ public class Emprestimo {
     private Ferramenta ferramenta;
     private Date dataInicio;
     private Date dataFim;
+
+    public Emprestimo(int id, Amigo amigo, Ferramenta ferramenta, Date dataInicio, Date dataFim) {
+        this.id = id;
+        this.amigo = amigo;
+        this.ferramenta = ferramenta;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+    }
+    
